30d43c8065733a541df0af1ea79d174a62703ae3
jp calvo <calvojp92@gmail.com>
2024-09-27 12:07:34 +0800

    fix: workaround build ReferrenceError in firefox

--- src/lib/tcplayer.ts
+++ src/lib/tcplayer.ts
@@ -2,6 +2,11 @@
 import TCPlayer from 'tcplayer.js';
 import type {TCPlayerApi, TCPlayerOptions} from './types.js';
 
+/* 🐞 Fixes build "ReferenceError" in Firefox */
+if (navigator.userAgent.search(/firefox/gi)) {
+  (globalThis as Record<string, unknown>).hlsSrc = null;
+}
+
 export function createTCPlayer(
   target: HTMLVideoElement,
   options: TCPlayerOptions,
