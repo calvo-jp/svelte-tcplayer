ab3d3f041fd9610bd9c098b73dfcbc5cad3b732c
jp calvo <calvojp92@gmail.com>
2024-09-27 13:38:28 +0800

    chore: add example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import {browser} from '$app/environment';
+  import '$lib/tcplayer.css';
+  import type {TCPlayerProps} from '$lib/types.js';
+  import type {Component} from 'svelte';
+
+  let TCPlayer: Component<TCPlayerProps>;
+
+  if (browser) {
+    import('$lib/tcplayer.svelte').then((m) => (TCPlayer = m.default));
+  } else {
+    import('$lib/ssr/tcplayer.svelte').then((m) => (TCPlayer = m.default));
+  }
+</script>
+
+<div class="container">
+  {#if TCPlayer}
+    <TCPlayer
+      options={{
+        licenseUrl:
+          'https://license.vod-pro.com/license/v2/1329648202_1/v_cube.license',
+        sources: [
+          {
+            src: 'webrtc://global-lebtest-play.myqcloud.com/live/lebtest?txSecret=f22a813b284137ed10d3259a7b5c224b&txTime=69f1eb8c&tabr_bitrates=d1080p,d540p,d360p&tabr_start_bitrate=d1080p',
+          },
+        ],
+        width: 1920 / 3,
+        height: 1080 / 3,
+        language: 'en-US',
+      }}
+    />
+  {/if}
+</div>
+
+<style>
+  :global(*, *::after, *::before) {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .container {
+    padding: 2rem;
+  }
+</style>
